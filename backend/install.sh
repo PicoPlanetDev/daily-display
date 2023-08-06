9f6ce62e05596111ec46404881c1eb8260f4e3b1
Sigmond Kukla <picoplanetdev@gmail.com>
2023-08-06 17:19:56 -0400

    Added install.sh to download thermal printer library

---
+++ backend/install.sh
@@ -0,0 +1,3 @@
+echo "Installing backend dependencies..."
+echo "Downloading Adafruit Thermal Printer Library..."
+wget -O Adafruit_Thermal.py https://raw.githubusercontent.com/adafruit/Python-Thermal-Printer/master/Adafruit_Thermal.py
