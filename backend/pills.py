f5f796a3650363ff069b4819fd256a180f1cd269
Sigmond Kukla <picoplanetdev@gmail.com>
2023-08-07 15:08:45 -0400

    Fixed issue with multiple threads using cursor

--- backend/pills.py
+++ backend/pills.py
@@ -4,23 +4,25 @@ import sqlite3
 class PillDatabase():
     def __init__(self):
         self.conn = sqlite3.connect('pills.db', check_same_thread=False)
-        self.cur = self.conn.cursor()
         self.ensure_database()
     
+    def get_cursor(self):
+        return self.conn.cursor()
+
     def ensure_database(self):
         # Create the table if it doesn't exist
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS pills
+        self.get_cursor().execute('''CREATE TABLE IF NOT EXISTS pills
             (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, round TEXT, taken INTEGER, number INTEGER, dispenser INTEGER)''')
         self.conn.commit()
 
         # Create the rounds table if it doesn't exist
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS rounds
+        self.get_cursor().execute('''CREATE TABLE IF NOT EXISTS rounds
             (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, time TEXT)''')
         self.conn.commit()
     
     def get_pills(self):
-        self.cur.execute("SELECT * FROM pills")
-        result = self.cur.fetchall()
+        self.get_cursor().execute("SELECT * FROM pills")
+        result = self.get_cursor().fetchall()
         pills = []
         for pill in result:
             pills.append({
@@ -34,8 +36,8 @@ class PillDatabase():
         return pills
     
     def get_rounds(self):
-        self.cur.execute("SELECT * FROM rounds")
-        result = self.cur.fetchall()
+        self.get_cursor().execute("SELECT * FROM rounds")
+        result = self.get_cursor().fetchall()
         rounds = []
         for round in result:
             rounds.append({
@@ -46,29 +48,29 @@ class PillDatabase():
         return rounds
     
     def add_pill(self, name, round, number, dispenser):
-        self.cur.execute("INSERT INTO pills (name, round, taken, number, dispenser) VALUES (?, ?, ?, ?, ?)", (name, round, 0, number, dispenser))
+        self.get_cursor().execute("INSERT INTO pills (name, round, taken, number, dispenser) VALUES (?, ?, ?, ?, ?)", (name, round, 0, number, dispenser))
         self.conn.commit()
 
     def add_round(self, name, time):
-        self.cur.execute("INSERT INTO rounds (name, time) VALUES (?, ?)", (name, time))
+        self.get_cursor().execute("INSERT INTO rounds (name, time) VALUES (?, ?)", (name, time))
         self.conn.commit()
     
     def delete_pill(self, id):
-        self.cur.execute("DELETE FROM pills WHERE id=?", (id,))
+        self.get_cursor().execute("DELETE FROM pills WHERE id=?", (id,))
         self.conn.commit()
     
     def delete_round(self, id):
-        self.cur.execute("DELETE FROM rounds WHERE id=?", (id,))
+        self.get_cursor().execute("DELETE FROM rounds WHERE id=?", (id,))
         self.conn.commit()
     
     def update_pill(self, id, name, round, number, dispenser):
-        self.cur.execute("UPDATE pills SET name=?, round=?, number=?, dispenser=? WHERE id=?", (name, round, number, dispenser, id))
+        self.get_cursor().execute("UPDATE pills SET name=?, round=?, number=?, dispenser=? WHERE id=?", (name, round, number, dispenser, id))
         self.conn.commit()
     
     def update_round(self, id, name, time):
-        self.cur.execute("UPDATE rounds SET name=?, time=? WHERE id=?", (name, time, id))
+        self.get_cursor().execute("UPDATE rounds SET name=?, time=? WHERE id=?", (name, time, id))
         self.conn.commit()
     
     def set_pill_status(self, id, taken):
-        self.cur.execute("UPDATE pills SET taken=? WHERE id=?", (taken, id))
+        self.get_cursor().execute("UPDATE pills SET taken=? WHERE id=?", (taken, id))
         self.conn.commit()
