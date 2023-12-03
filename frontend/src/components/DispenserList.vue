c6249a2fac73c9de9b43be9f8fc4dbf23af385b1
Sigmond Kukla <picoplanetdev@gmail.com>
2023-12-03 13:49:03 -0500

    Fixed help message

--- frontend/src/components/DispenserList.vue
+++ frontend/src/components/DispenserList.vue
@@ -111,7 +111,9 @@
                         <span class="input-group-text">Sensor pin</span>
                         <input type="number" class="form-control" placeholder="1" aria-label="Sensor pin"
                             v-model="editDispenserForm.sensor_pin" step="1">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        <div id="pinHelp" class="form-text">The board's pins are numbered left to right, from top to bottom,
+                            if you orient the board with the GPIO header (double row of pins) on the left, and the USB,
+                            HDMI, and Ethernet ports facing toward you.</div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-danger me-2" data-bs-dismiss="modal">Cancel</button>
