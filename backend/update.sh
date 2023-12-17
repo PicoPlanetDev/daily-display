b54d4c34b32ae5d72aa7f8919e411e921167da82
Sigmond Kukla <picoplanetdev@gmail.com>
2023-12-17 09:36:09 -0500

    Try only restarting services

--- backend/update.sh
+++ backend/update.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# Stop the services
-systemctl stop daily-display-backend.service
-systemctl stop daily-display-frontend.service
-
 # Pull the latest changes
 # Hacky way to run as orangepi user
 su orangepi -c "git pull"
 
 # Restart the services
-systemctl start daily-display-backend.service
-systemctl start daily-display-frontend.service
+systemctl restart daily-display-frontend.service
+systemctl restart daily-display-backend.service
