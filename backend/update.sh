2bb80ffd426b8dc9dc6be0c2097df5edbcb6863e
Sigmond Kukla <picoplanetdev@gmail.com>
2023-12-17 09:32:15 -0500

    Fix update.sh connecting to github

--- backend/update.sh
+++ backend/update.sh
@@ -5,7 +5,8 @@ systemctl stop daily-display-backend.service
 systemctl stop daily-display-frontend.service
 
 # Pull the latest changes
-git pull
+# Hacky way to run as orangepi user
+su orangepi -c "git pull"
 
 # Restart the services
 systemctl start daily-display-backend.service
