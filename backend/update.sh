76a2fb0fdd65413ff87386fab8609428f39da914
Sigmond Kukla <picoplanetdev@gmail.com>
2023-12-17 10:03:59 -0500

    Add echos to update script

--- backend/update.sh
+++ backend/update.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Updating Daily Display..." # log
+
 # Pull the latest changes
 # Hacky way to run as orangepi user
 su orangepi -c "git pull"
 
 # Restart the services
+echo "Restarting services..." # log
 systemctl restart daily-display-frontend.service
 systemctl restart daily-display-backend.service
